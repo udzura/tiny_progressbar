49704278d88d22bcb32de6f919c541172f435cc2
Uchio KONDO <udzura@udzura.jp>
2013-02-28 10:45:00 +0900

    Add documents

--- lib/tiny_progressbar.rb
+++ lib/tiny_progressbar.rb
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 require "tiny_progressbar/version"
 
 class TinyProgressbar
   LF = "\r"
   FORMAT = "[%-30s] (%03d/%03d done)"
 
+  # プログレスバーを初期化する。
+  # 引数として、処理対象となるオブジェクトの個数を渡す。
+  # @param [Integer] goal 処理対象のオブジェクト個数
   def initialize(goal)
     @goal = goal
     @now = 0
@@ -11,6 +15,8 @@ class TinyProgressbar
   end
   attr_reader :goal, :now
 
+  # プログレスバーを1ポイント進捗させる。
+  # 同時に、進捗状況を表示する。
   def succeed
     @now += 1
     back_cursor
--- tiny_progressbar.gemspec
+++ tiny_progressbar.gemspec
@@ -18,4 +18,6 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
 
   gem.add_development_dependency 'rake'
+  gem.add_development_dependency 'yard'
+  gem.add_development_dependency 'redcarpet'
 end
